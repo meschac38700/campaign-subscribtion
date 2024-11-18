1dce864418a26c694f7b3788c0d1cfac953e61b2
Eliam LOTONGA <e.m.lotonga@gmail.com>
2024-11-18 01:23:41 +0100

    :technologist: Review FetchJSON to add profiler state behaviour

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,14 +1,21 @@
-
 interface FetchResponse<T>{
     json: T,
     status: number
+}
+
+type ProfilerState = {
+    setIsLoading: (value: boolean) => void;
+    setError: (value: string) => void;
 }
 
 /**
  * Manager Fetch asynchronous request
  * Can be used in client/server mode
  */
-export default async function fetchJSON<T>(url: string, options?: RequestInit): Promise<FetchResponse<T>> {
+export default async function fetchJSON<T>(url: string, options?: RequestInit, profiler?: ProfilerState): Promise<FetchResponse<T>> {
+    profiler?.setIsLoading?.(true)
+    let json = null
+    let status = 500
     const _options: RequestInit = {
         method: 'GET',
         headers: {
@@ -16,7 +23,16 @@ export default async function fetchJSON<T>(url: string, options?: RequestInit): 
         },
         ...(options || {})
     }
-    if (_options.method?.toUpperCase() !== "GET") _options["body"] = JSON.stringify(_options.body || {});
-    const res = await fetch(url, _options);
-    return { json: await res.json(), status: res.status };
+
+    try{
+        const res = await fetch(url, _options);
+        status = res.status;
+        json = await res.json();
+    }catch(error){
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        profiler?.setError?.(error.toString());
+    }
+    profiler?.setIsLoading?.(false)
+    return { json, status };
 }
