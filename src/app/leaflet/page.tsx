6aee942b632e17fae6476486db61d49d4ffcda37
Eliam LOTONGA <e.m.lotonga@gmail.com>
2024-11-27 23:26:19 +0100

    :hammer: Draw a circle based on start and end coordinates

--- src/app/leaflet/page.tsx
+++ src/app/leaflet/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import useLeafletMap from "@/hooks/map/use-leaflet-map";
-import {FeatureGroup, LayerGroup, Map} from "leaflet";
-import {useCallback, useEffect} from "react";
+import {FeatureGroup, LatLngLiteral, LayerGroup, Map} from "leaflet";
+import {useEffect} from "react";
 import useFetch from "@/hooks/use-fetch";
 import {addLegend} from "@/lib/map/legend";
 import {
@@ -11,11 +11,30 @@ import {
 import {PartialEstablishment} from "@/interfaces/establishment";
 import {getUserPosition, markerIcon} from "@/utils/maps";
 
+
+const getDistance = (map: Map, start: LatLngLiteral, end: LatLngLiteral) => {
+    const startPoint = map.latLngToLayerPoint(start)
+    const endPoint = map.latLngToLayerPoint(end)
+    return startPoint.distanceTo(endPoint)
+}
+
+const circleBetween = (map: Map, start: LatLngLiteral, end: LatLngLiteral) => {
+    const distance = getDistance(map, start, end)
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const circle = L.circleMarker(start, {radius: distance}).addTo(map)
+
+    map.on('zoomend', function() {
+        const distance = getDistance(map, start, end)
+        circle.setRadius(distance)
+    });
+}
+
 const markCurrentUserPosition = (map: Map) => {
     getUserPosition((userPosition) => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-expect-error
-        const icon = markerIcon(L, {
+        let icon = markerIcon(L, {
             iconUrl: "https://img.icons8.com/color/48/i-pronoun.png",
             iconSize: [48, 48],
             iconAnchor: [24, 48],
@@ -23,6 +42,27 @@ const markCurrentUserPosition = (map: Map) => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-expect-error
         L.marker(userPosition, {icon}).addTo(map)
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        icon = markerIcon(L, {
+            iconUrl: "https://img.icons8.com/stickers/50/dollar-place-marker.png",
+            iconSize: [50, 50],
+            iconAnchor: [25, 50],
+        })
+        const destination = {lat: 45.59438, lng: 4.76594}
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        L.marker(destination, {icon}).addTo(map)
+
+        circleBetween(map, userPosition, destination)
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        L.polyline([userPosition, destination], {
+            color: "#e7700d",
+            weight: 8,
+            dashArray: "5 20",
+        }).addTo(map)
 
     })
 }
