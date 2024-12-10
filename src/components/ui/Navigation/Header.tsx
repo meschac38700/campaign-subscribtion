629ee8438c429f0247cca3535d686e4ad9ed27a6
Eliam LOTONGA <e.m.lotonga@gmail.com>
2024-12-10 13:40:20 +0100

    :hammer: Flat leaflet navigation links

--- src/components/ui/Navigation/Header.tsx
+++ src/components/ui/Navigation/Header.tsx
@@ -28,22 +28,16 @@ export function Header() {
         <NavigationMenu>
             <NavigationMenuList>
                 <NavigationMenuItem>
-                    <NavigationMenuTrigger>Leaflet</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                            {leafletPages.map((component) => (
-                                <ListItem
-                                    key={component.title}
-                                    title={component.title}
-                                    href={component.href}
-                                >
-                                    {component.description}
-                                </ListItem>
-                            ))}
-                        </ul>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
+                    <Link href="/leaflet" legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Leaflet
+                        </NavigationMenuLink>
+                    </Link>
+                    <Link href="/leaflet/travel" legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Leaflet travel
+                        </NavigationMenuLink>
+                    </Link>
                     <Link href="/" legacyBehavior passHref>
                         <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                             Campaigns
